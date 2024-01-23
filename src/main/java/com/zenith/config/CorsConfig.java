f581c7bad2d1078cbc3fb2b80d4a70258f8389e8
Taras Peles <pelestaras@gmail.com>
2024-01-23 19:31:18 +0200

    Cors configuration

---
+++ src/main/java/com/zenith/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.zenith.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/ws", configuration);
+        return source;
+    }
+}
